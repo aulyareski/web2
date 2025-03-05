af4bedc2833fb8f7b869052a404cf8d7ced1d1f6
aulya <aulyareski@gmail.com>
2025-03-05 11:07:38 +0700

    first commit

---
+++ infowebphp/Welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Selamat Belajar PHP</title>
+    </head>
+    <body>
+    <h3>Selamat Belajar PHP</h3>
+    <?php
+    $_nama = "Ahmad Marzuki";
+    $_umur = "20";
+    $_berat = "60";
+    $_prodi = "Sistem Informasi";
+    $_kampus = "STT NF";
+    echo "Hello, selamat datang di PHP!";
+    ?>
+    <hr>
+    <?php
+        echo "Nama Saya $_nama.saya berumur". $_umur tahun;
+        echo "<br> dengan berat badan". $_berat kg;
+        echo "<br> Saya Sedang Menempuh Pendidikan di Prodi". $_prodi;
+    ?>
+    </body>
+</html>
---
+++ infowebphp/welcome_new.php
@@ -0,0 +1,14 @@
+<?php
+    $nama = "Ahmad Marzuki";
+    $umur = 20;
+    $berat = 60;
+    $prodi = "Sistem Informasi";
+    $kampus = "STT NF";
+
+    echo "Hello selamat datang di PHP!<br>";
+    echo "Nama Saya: " . $nama . "<br>";
+    echo "Saya berumur: " . $umur . " tahun<br>";
+    echo "Dengan berat badan: " . $berat . " kg<br>";
+    echo "Saya sedang menempuh pendidikan di Prodi: " . $prodi . "<br>";
+    echo "Kampus: " . $kampus . "<br>";
+?>
